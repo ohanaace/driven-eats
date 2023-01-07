06bdc4403b1e0d79463f4e67333c36b3bc1666cb
suelen <sreginaclsilva@gmail.com>
2023-01-07 15:21:45 -0300

    criei a mensagem codificada com o pedido e o valor

--- driveneats.js
+++ driveneats.js
@@ -2,19 +2,25 @@
     let prato;
     let drink;
     let sobremesa;
-    let precodoPrato;
-    let precodoDrink;
+    let precoDoPrato;
+    let precoDoDrink;
+    let precoDaSobremesa;
+    let nomeDoPrato;
+    let nomeDoDrink;
+    let nomeDaSobremesa;
+    let valorTotal = 0;
+    let mensagem;
    
     function selecionaPrato(divNoThis){
     const pratoAnterior = document.querySelector('.food .selecionado')
-    console.log(divNoThis.children[1].innerHTML);
-    precodoPrato = parseInt(divNoThis.children[3].children[0].innerHTML.replace('R$ ', ''));
     if(pratoAnterior !== null){
         pratoAnterior.classList.remove('selecionado')
     }
 
     divNoThis.classList.add("selecionado");
     const meuPrato = document.querySelector('.selecionado');
+    nomeDoPrato = divNoThis.children[1].innerHTML;
+    precoDoPrato = parseInt(divNoThis.children[3].children[0].innerHTML.replace('R$ ', ''));
     prato = meuPrato.innerHTML;
     fecharPedido();
 }
@@ -22,10 +28,11 @@ function selecionaDrink(divNoThis){
     const drinkAnterior = document.querySelector('.drink .selecionado');
     if(drinkAnterior !== null){
         drinkAnterior.classList.remove('selecionado');
-
     }
     divNoThis.classList.add("selecionado");
     const meuDrink = document.querySelector('.selecionado');
+    nomeDoDrink = divNoThis.children[1].innerHTML;
+    precoDoDrink = parseInt(divNoThis.children[3].children[0].innerHTML.replace('R$ ', ''));
     drink = meuDrink.innerHTML
     fecharPedido();
 }
@@ -36,6 +43,8 @@ function selecionaSobremesa(divNoThis){
     }
     divNoThis.classList.add("selecionado");
     const minhaSobremesa = document.querySelector('.selecionado');
+    nomeDaSobremesa = divNoThis.children[1].innerHTML;
+    precoDaSobremesa = parseInt(divNoThis.children[3].children[0].innerHTML.replace('R$ ', ''));
     sobremesa = minhaSobremesa.innerHTML;
     fecharPedido();
 }
@@ -44,10 +53,21 @@ function fecharPedido(){
                const botao = document.querySelector('button');
             botao.innerHTML = "Fechar pedido";
             botao.disabled = false;
-            console.dir(botao)
             botao.classList.add('fechar');
-            }
-            
+            valorTotal = precoDoPrato + precoDoDrink + precoDaSobremesa;
+            console.log(valorTotal);
+
+            mandarPedido();    
+        }
+}
+function mandarPedido(){
+    mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${nomeDoPrato}
+    - Bebida: ${nomeDoDrink}
+    - Sobremesa: ${nomeDaSobremesa}
+    Total: R$ ${valorTotal},00 `
+    let mensagemCodificada = encodeURI(mensagem);
+    
 }
 
    
