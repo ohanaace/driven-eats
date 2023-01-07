7b50f4be9a0d90b8cfdbf111218112b2b412293d
suelen <sreginaclsilva@gmail.com>
2023-01-07 00:46:10 -0300

    eu FINALMENTE conseguilinkar as funcoes e agora o botao fechar so habilita se tiver um item selecionado em cada categoria

--- driveneats.js
+++ driveneats.js
@@ -1,24 +1,49 @@
-function selecionaPrato(divNoThis){
+
+    let prato;
+    let drink;
+    let sobremesa;
+   
+    function selecionaPrato(divNoThis){
     const pratoAnterior = document.querySelector('.food .selecionado')
     if(pratoAnterior !== null){
         pratoAnterior.classList.remove('selecionado')
     }
 
-    divNoThis.classList.add("selecionado")
+    divNoThis.classList.add("selecionado");
+    const meuPrato = document.querySelector('.selecionado');
+    prato = meuPrato.innerHTML;
+    fecharPedido();
 }
 function selecionaDrink(divNoThis){
-    const drinkAnterior = document.querySelector('.drink .selecionado')
+    const drinkAnterior = document.querySelector('.drink .selecionado');
     if(drinkAnterior !== null){
-        drinkAnterior.classList.remove('selecionado')
-    }
+        drinkAnterior.classList.remove('selecionado');
 
-    divNoThis.classList.add("selecionado")
+    }
+    divNoThis.classList.add("selecionado");
+    const meuDrink = document.querySelector('.selecionado');
+    drink = meuDrink.innerHTML
+    fecharPedido();
 }
 function selecionaSobremesa(divNoThis){
-    const doceAnterior = document.querySelector('.dessert .selecionado')
+    const doceAnterior = document.querySelector('.dessert .selecionado');
     if(doceAnterior !== null){
-        doceAnterior.classList.remove('selecionado')
+        doceAnterior.classList.remove('selecionado');
     }
-
-    divNoThis.classList.add("selecionado")
+    divNoThis.classList.add("selecionado");
+    const minhaSobremesa = document.querySelector('.selecionado');
+    sobremesa = minhaSobremesa.innerHTML;
+    fecharPedido();
 }
+function fecharPedido(){
+    if(prato !== undefined && drink !== undefined && sobremesa !== undefined) {             
+               const botao = document.querySelector('button');
+                botao.classList.add('fechar');
+            botao.innerHTML = "Fechar pedido";
+     
+        
+            }
+    
+}
+
+   
